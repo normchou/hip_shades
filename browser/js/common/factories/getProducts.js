a184cfec37a54925b42fe2406a6be04883a0733b
normchou <normchou@users.noreply.github.com>
2015-04-25 15:32:22 -0400

    add sunglasses to prducts page

--- browser/js/common/factories/getProducts.js
+++ browser/js/common/factories/getProducts.js
@@ -1,16 +1,23 @@
-app.factory('GetProductsFactory', function ($http) {
+app.factory('GetProductsFactory', function ($http, $stateParams) {
 
     var getProducts = function () {
-        return $http.get('/api/product')
+        return $http.get('/api/products')
             .then(function (response) {
                 return response.data;
             })
     }
 
+    var getSingleProduct = function () {
+    	return $http.get('/api/products/' + $stateParams.productID)
+    		.then(function (response) {
+    			return response.data;
+    		})
+    }
 
     
     return {
-        getProducts: getProducts
+        getProducts: getProducts,
+        getSingleProduct: getSingleProduct
     };
 })
 
--- browser/js/products/products.js
+++ browser/js/products/products.js
@@ -5,21 +5,27 @@ app.config(function ($stateProvider) {
 	        url: '/products',
 	        templateUrl: 'js/products/products.html',
 	        controller: 'ProductsController'
+	    })
+	    .state('products.item', {
+	    	url: '/:productID',
+	    	templateUrl: 'js/products/productItem.html',
+	    	controller: 'ProductsController'
 	    });
-	    // .state('product.title', {
-	    // 	url: '/:title',
-	    // 	templateUrl: 'js/products/products.category.html',
-	    // 	controller: 'ProductsController'
-	    // });
 });
 
 
-app.controller('ProductsController', function ($scope, GetProductsFactory) { 
+app.controller('ProductsController', function ($scope, $stateParams, GetProductsFactory) { 
 
 		GetProductsFactory.getProducts()
 			.then( function (productList) {
-					$scope.products = productList;
+				$scope.products = productList;
 			})
+
+		GetProductsFactory.getSingleProduct()
+			.then( function (productItem) {
+				$scope.productItem = productItem;
+			})
+
 })
 
 
