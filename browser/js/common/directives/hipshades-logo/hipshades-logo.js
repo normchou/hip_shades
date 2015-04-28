4fa1805142e22c1650538943bb9d249e6777fad5
normchou <normchou@users.noreply.github.com>
2015-04-28 19:20:52 -0400

    disabled user authentication

---
+++ browser/js/common/directives/hipshades-logo/hipshades-logo.js
@@ -0,0 +1,9 @@
+// unable to get this work at the moment - NC 4/26/2015
+
+'use strict';
+app.directive('hipshadesLogo', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/hipshades-logo/hipshades-logo.html'
+    };
+});
