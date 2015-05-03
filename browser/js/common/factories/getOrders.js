eb12c26de42c0110fce0c00a946545d7e827dc48
Old Gobbo <ramnikarora@gmail.com>
2015-05-03 19:13:47 -0400

    Added the delete button to Order management

--- browser/js/common/factories/getOrders.js
+++ browser/js/common/factories/getOrders.js
@@ -2,10 +2,10 @@
 
 app.factory('Orders', function (AuthService, $http) {
 
+	// get all orders by admin
     var getAllOrders = function () {
 		return AuthService.getLoggedInUser().then(function(data) {
 			var url = '/api/users/' + data._id + '/allOrders';
-			console.log(url);
 	        return $http.get(url)
 		})
 		.then(function (response) {
@@ -13,7 +13,24 @@ app.factory('Orders', function (AuthService, $http) {
 		})
     }
 
+	// delete url: /api/users/_userID_/orders/_orderID_/delete
+	var deleteOrder = function(orderID) {
+		return AuthService.getLoggedInUser()
+			.then(function (data) {
+				var url = '/api/users/' + data._id + '/orders/' + orderID + '/delete'
+				return $http.delete(url)
+			})
+			.then(function (res) {
+				return res.data
+			})
+			.catch (function (err) {
+				console.log(err)
+				return err
+			})
+	};
+
     return {
-        getAllOrders: getAllOrders
-    };
-});
+        getAllOrders: getAllOrders,
+		deleteOrder: deleteOrder
+    }
+})
