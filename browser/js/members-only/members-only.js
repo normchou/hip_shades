ccdccc42a71e4e856160c6e2fcb47b41a615fec1
normchou <normchou@users.noreply.github.com>
2015-05-07 01:55:55 -0400

    partially completed user setting page

--- browser/js/members-only/members-only.js
+++ browser/js/members-only/members-only.js
@@ -1,55 +1,43 @@
 app.config(function ($stateProvider) {
 
-    $stateProvider.state('membersOnly', {
-        url: '/members-area',
-        templateUrl: 'js/members-only/members-only.html',
-        controller: 'MemberController',
-        resolve: {
-            orders: function(Orders) {
-                return Orders.getAllOrders()
+    $stateProvider
+        .state('membersOnly', {
+            url: '/members-area',
+            templateUrl: 'js/members-only/members-only.html',
+            controller: 'MemberController',
+            resolve: {
+                order: function(CartFactory) {
+                    return CartFactory.getCurrentUser()
+                },
+                user: function(AuthService) {
+                    return AuthService.getLoggedInUser();
+                }
+            },
+            // The following data.authenticate is read by an event listener that controls access to this state. Refer to app.js.
+            data: {          
+                authenticate: true
             }
-        },
-        // The following data.authenticate is read by an event listener
-        // that controls access to this state. Refer to app.js.
-        data: {
-            authenticate: true
+        })
+        .state('membersOnly.edit', {
+            url: '/:userID',
+            templateUrl: 'js/members-only/members-only-edit.html'
+        })
+});
+
+app.controller('MemberController', function($scope, order, user) {
+     
+    $scope.order = order;
+    $scope.user = user;
+
+});
+
+
+app.filter('orderStatus', function() {
+    return function(input) {
+        if (input === false) {
+            return "In Progress"
+        } else {
+            return "Completed"
         }
-    });
-
-});
-
-
-app.controller('MemberController', function($scope, $http, SecretStash, orders, AuthService) {
-     
-     SecretStash.getStash().then(function (stash) {
-                $scope.stash = stash;
-            });
-     
-     $scope.order = orders;
-
-
-    console.log('calling controller', AuthService.getLoggedInUser().then(function(data) {return data}))
-
-     // $http.get('/session', function(err, data) {
-     //    if (err) return console.log(err);
-     //    console.log('this is the data', data)
-     // }
-     // authservice for localhost:1337/session
-
-
-
+    }
 })
-
-app.factory('SecretStash', function ($http) {
-
-    var getStash = function () {
-        return $http.get('/api/members/secret-stash').then(function (response) {
-            return response.data;
-        });
-    };
-
-    return {
-        getStash: getStash
-    };
-
-});
