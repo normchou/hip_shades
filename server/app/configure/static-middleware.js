1696c0c2b6ae83ec21a5a94474c578698085d746
Old Gobbo <ramnikarora@gmail.com>
2015-05-09 12:09:28 -0400

    Debugging public routes for Heroku

--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -1,4 +1,5 @@
 "use strict";
+var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var favicon = require('serve-favicon');
@@ -9,6 +10,12 @@ module.exports = function (app) {
     var bowerPath = path.join(root, './bower_components');
     var publicPath = path.join(root, './public');
     var browserPath = path.join(root, './browser');
+
+	console.log('root = ', root);
+	fs.readFile('/jquery/dist/jquery.js', function (err, data) {
+		if (err) console.log(err);
+		console.log('Jquery file = ', data);
+	});
 
     app.use(favicon(app.getValue('faviconPath')));
     app.use(express.static(bowerPath));
