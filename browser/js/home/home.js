1952ea35577e1ca3c7072e45893adb345848794f
normchou <normchou@users.noreply.github.com>
2015-05-09 11:38:02 -0400

    new home page

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,8 +1,8 @@
 'use strict';
-app.config(function ($stateProvider) {
-    $stateProvider
-    	.state('home', {
-	        url: '/',
-	        templateUrl: 'js/home/home.html',
-	    });
-});
+// app.config(function ($stateProvider) {
+//     $stateProvider
+//     	.state('home', {
+// 	        url: '/',
+// 	        templateUrl: 'js/home/home.html',
+// 	    });
+// });
